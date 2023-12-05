14f295781eb7a7a4afe2417af6cb516c0bb5fb60
Vaibhav Kurhe <vrk.vlab@coeptech.ac.in>
2023-12-05 12:44:00 +0530

    updated

---
+++ experiment/simulation/src/questionJson.js
@@ -0,0 +1,147 @@
+
+
+QuestionsJSON = {
+		"data": {
+			"SEC": [{
+
+				"QUES": [{
+					
+					"QC": "There are -----types of optical encoder:",
+					"ANS": [{
+						"ANSID": false,
+						"content": "4"
+					}, {
+						"ANSID": false,
+						"content": "5"
+					}, {
+						"ANSID": false,
+						"content": "3"
+					},{
+						"ANSID": true,
+						"content": "2 "
+					}]
+				}]
+			}, {
+
+				"QUES": [{
+					
+					"QC": " Which of the following can be measured using tachometers?",
+					"ANS": [{
+						"ANSID": true,
+						"content": "Angular speed"
+					}, {
+						"ANSID": false,
+						"content": "Linear speed "
+					}, {
+						"ANSID": false,
+						"content": "Acceleration"
+					}, {
+						"ANSID": false,
+						"content": "Vibration"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Which of the following types of speed is measured by a tachometer encoder?",
+
+
+					"ANS": [{
+						"ANSID": true,
+						"content": "false pulses"
+					}, {
+						"ANSID": false,
+						"content": "single revolution in multiple track"
+					}, {
+						"ANSID": false,
+						"content": "exact position of the motor shaft"
+					}, {
+						"ANSID": false,
+						"content": "forward and reverse directions"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "What are single output channel incremental encoders used for?",
+					"ANS": [{
+						"ANSID": true,
+						"content": "Sense Speed (Tachometers) "
+					}, {
+						"ANSID": false,
+						"content": "Position Feedback"
+					}, {
+						"ANSID": false,
+						"content": "Sense Direction"
+					}, {
+						"ANSID": false,
+						"content": "one of the above."
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "A tachometer encoder has-----",
+
+
+					"ANS": [{
+						"ANSID": false,
+						"content": "two outputs"
+					}, {
+						"ANSID": false,
+						"content": "three outputs"
+					}, {
+						"ANSID": true,
+						"content": "one output"
+					}, {
+						"ANSID": false,
+						"content": "none of these"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Which of the following is an Optical Encoder?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Actuator"
+					}, {
+						"ANSID": true,
+						"content": "Transducer"
+					}, {
+						"ANSID": false,
+						"content": "Increases"
+					}, {
+						"ANSID": false,
+						"content": "Micro controller"
+					}]
+					
+				}]
+			}
+			]
+		}
+
+
+	}
+
+
+
+var ANSWERJSON = {
+			
+				"0" : "All the points mentioned",
+				"1" : "Set the standard output for the required limits of process parameter",
+				"2" : "Set majority of the points of the transmitter output in the operating region",
+				"3" : "Pump discharge flow and Outlet flow",
+				"4" : "Nothing is required to be changed",
+				"5" : "Nil"
+				
+			
+}
